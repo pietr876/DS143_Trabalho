44ad10b63a11132b2266b4f28ee132114aa34eb9
pietr876 <84190971+pietr876@users.noreply.github.com>
2022-05-02 23:29:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+//#include <conio.h>
 #include <ctype.h>
 #include <string.h>
 #include <math.h>
-
-//Declaração da estrutura usada no programa.
-//Arvore Binaria.
 
 char *getTermo(FILE *fp){
     char termo[100];
@@ -64,7 +61,7 @@ void ordena(Arvore *a, int N, int arr[], char* str[]){
         int x = 0, y;
         char* c;
 
-        while(arr[x] > a->qtd)
+        while(arr[x] >= a->qtd)
             x++;
 
         for (int i = x; i < N; i++){
@@ -90,7 +87,7 @@ void ordena(Arvore *a, int N, int arr[], char* str[]){
 
 int main(int argc, char *argv[]){
     int cont = 0, N;
-    char* arq = "C:/Users/Pietro Giacomitti/Documents/Small_texts/128mb.txt";
+    //char* arq = "C:/Users/Pietro Giacomitti/Documents/Small_texts/128mb.txt";
 
     Arvore *a;
     a = inserir(NULL, "A");
@@ -154,38 +151,48 @@ int main(int argc, char *argv[]){
                         cont1++;
                     cont2++;
                 }
-                if(cont1 > 0)
+                if(cont1 > 0){
                     num_pres++;
-                tf[i] = ((float)cont1/cont2);
-                doc_name[i] = argv[i+3];
+                    tf[i] = ((float)cont1/cont2);
+                    doc_name[i] = argv[i+3];
+                }
             }
             fclose(fp);
         }
-        float idf = log10((float)n_doc/num_pres), a = 0.0;
-
-        if(((float)n_doc/num_pres) > 1.f){
-            for(int i = 0; i < n_doc; i++){
-                tf[i] *= idf;
-            }
+        if(num_pres == 0){
+            printf("\n\tNenhum arquivo possui o termo \"%s\".\n", argv[2]);
+            exit(1);
+        }else if(num_pres == n_doc){
+            printf("\n\tTodos os arquivos sao relevantes ao termo \"%s\".\n", argv[2]);
+            exit(1);
         }
-        char* str;
-        for (int i = 0; i < n_doc; i++){
-            for (int j = i + 1; j < n_doc; j++){
-                if(tf[i] < tf[j]){
-                    a = tf[i];
-                    str = doc_name[i];
+        else{
+            float idf = log10((float)n_doc/num_pres), a = 0.0;
 
-                    tf[i] = tf[j];
-                    doc_name[i] = doc_name[j];
+            //if(((float)n_doc/num_pres) > 1.f){
+                for(int i = 0; i < n_doc; i++){
+                    tf[i] *= idf;
+                }
+            //}
+            char* str;
+            for(int i = 0; i < n_doc; i++){
+                for (int j = i + 1; j < n_doc; j++){
+                    if(tf[i] < tf[j]){
+                        a = tf[i];
+                        str = doc_name[i];
 
-                    tf[j] = a;
-                    doc_name[j] = str;
+                        tf[i] = tf[j];
+                        doc_name[i] = doc_name[j];
+
+                        tf[j] = a;
+                        doc_name[j] = str;
+                    }
                 }
             }
-        }
-        printf("\n\tRelevancia de documentos para \"%s\":\n", argv[2]);
-        for(int i = 0; i < n_doc; i++){
-            printf("\t%d - %s\n", i+1, doc_name[i]);
+            printf("\n\tRelevancia de documentos para \"%s\":\n", argv[2]);
+            for(int i = 0; i < n_doc; i++){
+                printf("\t%d - %s\n", i+1, doc_name[i]);
+            }
         }
     }else{
         printf("\n\t**Parametros invalidos passados em: ");
